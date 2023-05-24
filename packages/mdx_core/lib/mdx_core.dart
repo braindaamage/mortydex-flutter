a469ab1809591fa6091f95e2adb658b548bea0b5
Leonardo Olivares <leonardo.olivares@cotalker.com>
2023-05-24 01:58:08 -0300

    first commit core

---
+++ packages/mdx_core/lib/mdx_core.dart
@@ -0,0 +1,11 @@
+library mdx_core;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class HelloWorld {
+  static String helloWorld = 'Hello World!';
+}
