1dcdc6ed20484789ffe3afd6a0e8625ef502262a
Leonardo Olivares <leonardo.olivares@cotalker.com>
2023-05-25 22:58:07 -0300

    Merge pull request #1 from braindaamage/feature/logger-package
    Logger first versión

---
+++ packages/mdx_logger/test/mdx_logger_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mdx_logger/logger_provider.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:mdx_logger/mdx_logger.dart';
+
+/// Generate Mock with 'dart run build_runner build'
+@GenerateNiceMocks([MockSpec<LoggerProvider>()])
+import './mdx_logger_test.mocks.dart';
+
+void main() {
+  final mockLoggerProvider = MockLoggerProvider();
+
+  group("useLogger", () {
+    setUp(() {
+      Logger.addProvider(mockLoggerProvider);
+    });
+
+    test('should fordware the call to mockLoggerProvider', () {
+      // arrange
+      const String message = 'Test message';
+      const LogTypes type = LogTypes.info;
+      // act
+      Logger.log(message: message, type: type);
+      // assert
+      verify(mockLoggerProvider(message: message, type: type));
+    });
+  });
+}
