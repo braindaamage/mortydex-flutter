5ede6858605544f694ac425053b4ce6e564797f1
Leonardo Olivares <leonardo.olivares@cotalker.com>
2023-05-26 17:57:44 -0300

    new better implementation of di container

--- packages/mdx_dicontainer/lib/mdx_dicontainer.dart
+++ packages/mdx_dicontainer/lib/mdx_dicontainer.dart
@@ -1,43 +1,16 @@
 library mdx_dicontainer;
 
-abstract class MDXDIContainerProtocol {
-  MDXDIContainer get instance;
+part 'mdx_dicontainer_imp.dart';
+
+abstract class MDXDIContainer {
+  static final _instance = _MDXDIContainerImplementation();
+  static MDXDIContainer get instance => _instance;
+  static MDXDIContainer get I => _instance;
   Service call<Service extends Object>();
   Service resolve<Service extends Object>();
-  void register<Service extends Object>({required Service instance});
-}
-
-class MDXDIContainer implements MDXDIContainerProtocol {
-  static final MDXDIContainer _instance = MDXDIContainer._internal();
-
-  @override
-  MDXDIContainer get instance => _instance;
-
-  factory MDXDIContainer() {
-    return _instance;
-  }
-
-  MDXDIContainer._internal();
-
-  final Map<String, dynamic> _services = {};
-
-  void _register<Service extends Object>({
-    required Service instance,
-    required String name,
-  }) {
-    _services[name] = instance;
-  }
-
-  Service _resolve<Service extends Object>() => _services[Service.toString()];
-
-  @override
-  Service call<Service extends Object>() => _instance._resolve();
-
-  @override
-  Service resolve<Service extends Object>() => _instance._resolve();
-
-  @override
-  void register<Service extends Object>({required Service instance}) {
-    _instance._register(instance: instance, name: Service.toString());
-  }
+  void registerFactory<Service extends Object>(FactoryCreator<Service> creator);
+  void registerLazySingleton<Service extends Object>(
+      FactoryCreator<Service> creator);
+  void registerSingleton<Service extends Object>(
+      FactoryCreator<Service> creator);
 }
