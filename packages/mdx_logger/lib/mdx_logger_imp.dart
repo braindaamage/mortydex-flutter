2e209054635303f2197ffbc7ff9eee7a336a96f5
Leonardo Olivares <leonardo.olivares@cotalker.com>
2023-05-26 18:53:44 -0300

    better logger implementation (#7)

---
+++ packages/mdx_logger/lib/mdx_logger_imp.dart
@@ -0,0 +1,33 @@
+part of 'mdx_logger.dart';
+
+enum LogTypes {
+  info,
+  warning,
+  error,
+}
+
+extension ToString on LogTypes {
+  String toShortString() => toString().split('.').last.toUpperCase();
+}
+
+class _Logger implements Logger {
+  final Set<LoggerProvider> _providers = {};
+
+  _Logger(List<LoggerProvider> providers) {
+    for (var provider in providers) {
+      _addProvider(provider);
+    }
+  }
+
+  @override
+  void _log({required String message, required LogTypes type}) {
+    for (var provider in _providers) {
+      provider(message: message, type: type);
+    }
+  }
+
+  @override
+  void _addProvider(LoggerProvider provider) {
+    _providers.add(provider);
+  }
+}
