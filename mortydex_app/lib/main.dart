1478a96a4e5842e7b2e1334c77a27d5f415e3229
Leonardo Olivares <leonardo.olivares@gmail.com>
2023-06-01 23:57:36 -0300

    remove old packages

--- mortydex_app/lib/main.dart
+++ mortydex_app/lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:mdx_core/mdx_core.dart';
-import 'package:mdx_logger/mdx_logger.dart';
+// import 'package:mdx_core/mdx_core.dart';
 
-void main() => runApp(const MainApp());
+void configureDependencies() {
+  // core.configureDependencies(resolverDI);
+}
+
+void main() {
+  configureDependencies();
+  return runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Logger.log(message: 'Hello Logger!', type: LogTypes.info);
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(HelloWorld.helloWorld),
+          child: Text('HelloWorld'),
         ),
       ),
     );
   }
+}
+
+abstract interface class TextClassProtocol {
+  String text;
+  TextClassProtocol(this.text);
+  String hola();
+}
+
+class TextClassImp implements TextClassProtocol {
+  @override
+  String text;
+
+  TextClassImp(this.text);
+
+  @override
+  String hola() => text;
 }
